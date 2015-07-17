1e66a83abc9001077cf9eeb82d8450f660988123
Robert Gogolok <gogolok@gmail.com>
2015-07-17 14:12:01 +0200

    hint where to find plugin docu

--- main.go
+++ main.go
@@ -269,22 +269,7 @@ func (c *CfServiceJumperPlugin) GetMetadata() plugin.PluginMetadata {
 	}
 }
 
-/**
- * Unlike most Go programs, the `Main()` function will not be used to run all of the
- * commands provided in your plugin. Main will be used to initialize the plugin
- * process, as well as any dependencies you might require for your
- * plugin.
- */
+// https://github.com/cloudfoundry/cli/tree/master/plugin_examples
 func main() {
-	// Any initialization for your plugin can be handled here
-	//
-	// Note: to run the plugin.Start method, we pass in a pointer to the struct
-	// implementing the interface defined at "github.com/cloudfoundry/cli/plugin/plugin.go"
-	//
-	// Note: The plugin's main() method is invoked at install time to collect
-	// metadata. The plugin will exit 0 and the Run([]string) method will not be
-	// invoked.
 	plugin.Start(new(CfServiceJumperPlugin))
-	// Plugin code should be written in the Run([]string) method,
-	// ensuring the plugin environment is bootstrapped.
 }
