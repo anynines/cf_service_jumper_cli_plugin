fdb0142a7a9ffafa3455b4d660364fd93562131a
Robert Gogolok <gogolok@gmail.com>
2015-11-11 09:41:21 +0100

    concourse ci support

---
+++ bin/ci-test.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+# vim: set ft=sh
+
+set -e
+
+export GOPATH=$PWD/gopath
+export PATH=$GOPATH/bin:$PATH
+
+cd $GOPATH/src/github.com/a9hcp/cf_service_jumper_cli_plugin
+
+export GOPATH=${PWD}/Godeps/_workspace:$GOPATH
+export PATH=${PWD}/Godeps/_workspace/bin:$PATH
+
+go install github.com/onsi/ginkgo/ginkgo
+
+ginkgo -r "$@"
