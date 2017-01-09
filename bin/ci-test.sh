65a470baff1c6cc48ce020fe2697ecb4da8c0f8a
Robert Gogolok <gogolok@gmail.com>
2017-01-09 18:02:44 +0100

    adjust concourse config to new version

--- bin/ci-test.sh
+++ bin/ci-test.sh
@@ -1,13 +1,11 @@
 #!/bin/bash
 # vim: set ft=sh
 
-set -e
+set -eu
 
 export GOPATH=$PWD/gopath
 export PATH=$GOPATH/bin:$PATH
 
 cd $GOPATH/src/github.com/anynines/cf_service_jumper_cli_plugin
 
-go install github.com/onsi/ginkgo/ginkgo
-
-ginkgo -r "$@"
+go test
